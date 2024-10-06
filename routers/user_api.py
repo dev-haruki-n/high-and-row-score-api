9d58df33454bb1122de76f50a01ecc8d4f406a1f
dev-haruki-n <dev.haruki.n@gmail.com>
2024-10-06 19:00:10 +0900

    ミスの修正とエンドポイントの追加

--- routers/user_api.py
+++ routers/user_api.py
@@ -2,17 +2,24 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/user")
-def user_get():
-    return {
+records = {
         "records": [
             {
-                "name:": "Adam",
+                "name": "Adam",
                 "score": 100
                 },
             {
-                "name:": "Ban",
+                "name": "Ban",
                 "score": 200
             }
         ]
     }
+
+@router.get("/user")
+def get_records():
+    return records
+
+@router.get("/user/{id}")
+def get_record_by_id(id: int):
+    record = records["records"][id]
+    return record
